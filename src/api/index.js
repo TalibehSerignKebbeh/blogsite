52ddc74278db92781c68e9850231f931c4210f54
Modou Mbye <mm21821027@ut.edu.gm>
2023-05-15 17:51:49 +0000

    commit for updating api url

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,9 @@
 import axios from "axios"
 
-
-export const apiUrl = 'http://localhost:6002/api/v1'
-export const ImageUrl = 'http://localhost:6002'
+export const apiUrl='https://blog-api-hnde.onrender.com/v1'
+export const ImageUrl='https://blog-api-hnde.onrender.com'
+// export const apiUrl = 'http://localhost:6002/api/v1'
+// export const ImageUrl = 'http://localhost:6002'
 
 
 export const AxiosInstance = axios.create({ baseURL: apiUrl })
